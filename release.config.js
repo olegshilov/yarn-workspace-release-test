dfe27d08bb0521cef0e80f1dfbcb503c699f6be1
Oleg Shilov <me@olegshilov.com>
2021-07-16 10:25:11 +0300

    chore: new release commit message

--- release.config.js
+++ release.config.js
@@ -1,23 +1,16 @@
 module.exports = {
   branches: ["main", "next", "dev"],
+  preset: "conventionalcommits",
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "conventionalcommits",
-      },
-    ],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        preset: "conventionalcommits",
-      },
-    ],
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "package.json"],
+        assets: ["CHANGELOG.md", "package.json", "yarn.lock"],
+        message:
+          "chore(release): ${nextRelease.gitTag}\n\n${nextRelease.notes}\n\n[skip ci]",
       },
     ],
     "@semantic-release/npm",
