ce99bbc2aa838dcd32907bf31249210f214d5320
Oleg Shilov <me@olegshilov.com>
2021-07-16 11:09:01 +0300

    feat(pack-2): add more logs

--- packages/pack-2/src/index.ts
+++ packages/pack-2/src/index.ts
@@ -3,6 +3,7 @@ console.log("init");
 const timeout = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 (async function () {
+  console.log("log before timeout");
   await timeout(5000);
   console.log("log after timeout");
 })();
