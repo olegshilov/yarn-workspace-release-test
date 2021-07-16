c3dcebb07e21abcd9241ae4a14d4394d225ef1d0
Oleg Shilov <me@olegshilov.com>
2021-07-16 10:32:07 +0300

    feat(pack-2): log after timeout

--- packages/pack-2/src/index.ts
+++ packages/pack-2/src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello");
+console.log("init");
+
+const timeout = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+(async function () {
+  await timeout(5000);
+  console.log("log after timeout");
+})();
